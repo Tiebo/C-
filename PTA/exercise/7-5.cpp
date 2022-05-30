d0976b3f2bccb564efbfeaa5fce4c9870333f295
YH H <2702194875@qq.com>
2022-05-30 16:37:02 +0800

    nei

--- PTA/exercise/7-5.cpp
+++ PTA/exercise/7-5.cpp
@@ -1,28 +1,38 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 int main()
 {
-    int n,count=0,i;
-    cin>>n;
+    int n, count = 0, i;
+    cin >> n;
     float a[n];
-    for(int i=0; i<n; i++)
-        cin>>a[i];
-    if(n==1){cout<<"1";return 0;}
-    sort(a,a+n);
-    for(i=1;i<n;i++)
-    {   
-        if(a[i]+a[i-1]<=3&&a[i-1]!=4){
-            a[i]=4;a[i-1]=4;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    if (n == 1)
+    {
+        cout << "1";
+        return 0;
+    }
+    sort(a, a + n);
+    for (i = 1; i < n; i++)
+    {
+        if (a[i] + a[i - 1] <= 3 && a[i - 1] != 4)
+        {
+            a[i] = 4;
+            a[i - 1] = 4;
             count++;
         }
-        if(a[i]+a[i-1]>3&&a[i-1]!=4)
+        if (a[i] + a[i - 1] > 3 && a[i - 1] != 4)
             break;
     }
-    if(count==0||n==0){cout<<""<<n;}
-    else{
-    count=n-i+count+1;
-    cout<<""<<count;
+    if (count == 0 || n == 0)
+    {
+        cout << "" << n;
+    }
+    else
+    {
+        count = n - i + count + 1;
+        cout << "" << count;
     }
     return 0;
 }
